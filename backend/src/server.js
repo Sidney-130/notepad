2a4970c4267c9445bbe16b9724a7330ee559730c
Sidney-130 <akabuoguoluebube8@gmail.com>
2026-01-05 23:37:23 +0100

    update server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
-const app = require("./app");
 require("dotenv").config();
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
